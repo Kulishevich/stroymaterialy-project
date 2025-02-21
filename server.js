f2a8c8e4354825b1f6b9f8497d70555633b7426c
Maksim <maxim8992012@gmail.com>
2025-02-21 14:05:43 +0300

    feat: add server

--- server.js
+++ server.js
@@ -1,38 +1,23 @@
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
- 
-const dev = process.env.NODE_ENV !== 'production'
-const hostname = 'localhost'
-const port = 3000
+const { createServer } = require("https");
+const { parse } = require("url");
+const next = require("next");
+const fs = require("fs");
 
-const app = next({ dev, hostname, port })
-const handle = app.getRequestHandler()
- 
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const httpsOptions = {
+  key: fs.readFileSync("./certs/key.pem"), // путь к ключу
+  cert: fs.readFileSync("./certs/cert.pem"), // путь к сертификату
+};
+
 app.prepare().then(() => {
-  createServer(async (req, res) => {
-    try {
-      const parsedUrl = parse(req.url, true)
-      const { pathname, query } = parsedUrl
- 
-      if (pathname === '/a') {
-        await app.render(req, res, '/a', query)
-      } else if (pathname === '/b') {
-        await app.render(req, res, '/b', query)
-      } else {
-        await handle(req, res, parsedUrl)
-      }
-    } catch (err) {
-      console.error('Error occurred handling', req.url, err)
-      res.statusCode = 500
-      res.end('internal server error')
-    }
-  })
-    .once('error', (err) => {
-      console.error(err)
-      process.exit(1)
-    })
-    .listen(port, () => {
-      console.log(`> Ready on http://${hostname}:${port}`)
-    })
-})
+  createServer(httpsOptions, (req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(3000, (err) => {
+    if (err) throw err;
+    console.log("🚀 Server running on https://localhost:3000");
+  });
+});
