f1d32eb5c8b230d3c3880afbc0d6a96878e22950
Maksim Kulishevich <maxim8992012@gmail.com>
2025-05-19 19:34:56 +0300

    feat: add translations in seccess order page

--- src/features/OrderSuccess/OrderSuccess.tsx
+++ src/features/OrderSuccess/OrderSuccess.tsx
@@ -1,33 +1,59 @@
-import { SuccessIcon } from "@/shared/assets/icons";
+import { ErrorIcon, SuccessIcon } from "@/shared/assets/icons";
 import React from "react";
 import s from "./OrderSuccess.module.scss";
 import { Typography } from "@/shared/ui/typography";
 import { Button } from "@/shared/ui/button";
 import Link from "next/link";
 import { Paths } from "@/shared/enums";
+import { useRouter } from "next/router";
+import { useGetOrderQuery } from "@/api/orders/orders.api";
+import { useTranslations } from "next-intl";
 
 export const OrderSuccess = () => {
-  return (
+  const t = useTranslations("orderSuccess");
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { data } = useGetOrderQuery({ id: (id as string) || "" });
+
+  return !!data ? (
     <div className={s.container}>
-      <SuccessIcon />
+      <SuccessIcon className={s.green} />
       <div className={s.textContent}>
         <Typography variant="h2" as="h2">
-          Ваш заказ оформлен!
+          {t("successTitle")}
         </Typography>
-        {/* <Typography variant="body_1">
-          Номер вашего заказа: <strong>12345</strong>
-        </Typography> */}
-        <Typography variant="body_3">
-          Скоро с вами свяжется наш менеджер для подтверждения заказа
+        <Typography variant="body_1">
+          {t("orderNumber")} <strong>{data?.data.id}</strong>
         </Typography>
+        <Typography variant="body_3">{t("managerWillCall")}</Typography>
       </div>
 
       <div className={s.buttonsContainer}>
         <Button as={Link} href={`${Paths.profile}?tab=orders`}>
-          Заказы
+          {t("ordersBtn")}
         </Button>
         <Button variant="secondary" as={Link} href={"/"}>
-          Перейти на главную
+          {t("homeBtn")}
+        </Button>
+      </div>
+    </div>
+  ) : (
+    <div className={s.container}>
+      <ErrorIcon className={s.red} />
+      <div className={s.textContent}>
+        <Typography variant="h2" as="h2">
+          {t("errorTitle")}
+        </Typography>
+        <Typography variant="body_3">{t("managerWillCallError")}</Typography>
+      </div>
+
+      <div className={s.buttonsContainer}>
+        <Button as={Link} href={`${Paths.profile}?tab=orders`}>
+          {t("ordersBtn")}
+        </Button>
+        <Button variant="secondary" as={Link} href={"/"}>
+          {t("homeBtn")}
         </Button>
       </div>
     </div>
